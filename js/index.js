3527e03afe0c5a8bba80816dc2196826bed6259b
Jared Womack <womackjw@hotmail.com>
2019-05-21 13:23:46 -0700

    Added 4 event listeners

--- js/index.js
+++ js/index.js
@@ -1,1 +1,34 @@
 // Your code goes here
+
+//MouseOver EventListener
+const navLogo = document.querySelector('.logo-heading');
+navLogo.addEventListener('mouseover', event => {
+    event.target.style.fontSize = '30px';
+    event.target.style.fontWeight = '200';
+    event.target.style.color = 'green';
+}); 
+
+//Make Home button Opaque
+const homeAuxClick = document.querySelector('nav a');
+homeAuxClick.addEventListener('auxclick', event => {
+    event.target.style.opacity = '0.5';
+});
+
+//Make head image circular and add a border on mouseenter
+const busImg = document.getElementById('header-img');
+busImg.addEventListener('mouseenter', event => {
+    event.target.style.borderRadius = '50%'; 
+    event.target.style.border = '3px solid black'; 
+});
+//Make image return to rectangle with grey border
+busImg.addEventListener('mouseleave', event => {
+    event.target.style.border = '3px solid grey'; 
+    event.target.style.borderRadius = '0%';
+})
+
+
+
+
+
+
+
